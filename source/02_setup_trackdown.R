59d9438ca4cd433081cf0764331e8cb4bbe96041
ADOLF <janne.adolf@inbo.be>
2026-08-21 17:58:48 +0200

    update script for trackdown setup

--- source/02_setup_trackdown.R
+++ source/02_setup_trackdown.R
@@ -1,2 +1,8 @@
-# install dev version of trackdown (needed for quarto)
+# install trackdown from github:
+# current CRAN version is 1.1.1,
+# but higher version (>=1.3.1) required for Quarto files
+# see https://github.com/ClaudioZandonella/trackdown/blob/main/README.md
 remotes::install_github("claudiozandonella/trackdown", build_vignettes = TRUE)
+
+# authorize access to g-drive:
+# https://claudiozandonella.github.io/trackdown/articles/oauth-client-configuration.html
